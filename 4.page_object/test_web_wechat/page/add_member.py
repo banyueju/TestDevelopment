d576d3a3fb48b5e152dffc45c5939da868b7aabe
banyueju <81305994+banyueju@users.noreply.github.com>
2021-05-08 16:04:39 +0800

    Update add_member.py

--- 4.page_object/test_web_wechat/page/add_member.py
+++ 4.page_object/test_web_wechat/page/add_member.py
@@ -2,9 +2,10 @@
 __author__ = '伴月雎'
 __time__ = '2021/5/7 23:09'
 """
+from test_web_wechat.page.contact import ContactPage
 
 
 class AddMemberPage:
 
     def add_member(self):
-        pass
+        return ContactPage()
