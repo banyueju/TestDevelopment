1f12bed8aa8cd815e94e5e89282993273f492b20
banyueju <81305994+banyueju@users.noreply.github.com>
2021-05-08 16:03:35 +0800

    Update test_add_member.py

--- 4.page_object/test_web_wechat/testcase/test_add_member.py
+++ 4.page_object/test_web_wechat/testcase/test_add_member.py
@@ -2,8 +2,9 @@
 __author__ = '伴月雎'
 __time__ = '2021/5/7 23:28'
 """
+from test_web_wechat.page.main_page import MainPage
 
 
 class TestAddMember:
     def test_add_member(self):
-        pass
+        MainPage().goto_add_member().add_member().get_contact_list()
